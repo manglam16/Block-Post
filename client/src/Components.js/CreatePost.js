5373e8426b3cbbff49baef46c386e3b72092e17c
manglam16 <paliwalmanglam16@gmail.com>
2023-05-17 16:21:58 +0530

    minor changes

--- client/src/Components.js/CreatePost.js
+++ client/src/Components.js/CreatePost.js
@@ -1,35 +1,29 @@
 import React, { useState } from "react";
 import { TextField, Button, Box, Stack } from "@mui/material";
-import Web3 from "web3"; // Import Web3.js library
+import Web3 from "web3";
 
-// Contract ABI and address
-const contractABI = ""; // Replace with the ABI of your Solidity contract
-const contractAddress = "0x..."; // Replace with the address of your deployed contract
+const contractABI = "";
+const contractAddress = "0x...";
 
 function CreatePost() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [author, setAuthor] = useState("");
 
-  // Function to handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Connect to the Ethereum network using Web3
     if (window.ethereum) {
       await window.ethereum.request({ method: "eth_requestAccounts" });
       const web3 = new Web3(window.ethereum);
 
       try {
-        // Get the contract instance
         const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-        // Send the post data to the contract
         await contract.methods
           .createPost(title, content, author)
           .send({ from: web3.eth.defaultAccount });
 
-        // Clear the form fields
         setTitle("");
         setContent("");
         setAuthor("");
